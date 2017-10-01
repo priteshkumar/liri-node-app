2dcfb61d38d0ab642ae5e6e4f4ec4f9e0af96a99
Pritesh <pritesh.kumar2@gmail.com>
2017-09-30 23:05:55 -0700

    add twitter result log

--- liri.js
+++ liri.js
@@ -6,21 +6,30 @@ var twitterClient = new twitterOb(Keys.twitterKeys);
 var spotifyClient = new spotifyOb(Keys.spotifyKeys);
 
 
+var command = process.argv[2];
+var cmdparam = process.argv[3];
+
+
+if(command === "my-tweets" || command === "mytweets" || command === "Mytweets"){
 console.log("calling twitter rest endpoint");
 
 twitterClient.get('statuses/user_timeline',{user_id:"mavpks",count:20},function(error,tweets,response){
  for(var i=0;i < tweets.length;i++){
- 	//console.log("#######  Tweet: " + (i+1) + " ########");
- 	//console.log("Posted at: " + tweets[i].created_at);
- 	//console.log("Tweet text: " + tweets[i].text);
- 	//console.log("");
+ 	console.log("#######  Tweet: " + (i+1) + " ########");
+ 	console.log("Posted at: " + tweets[i].created_at);
+ 	console.log("Tweet text: " + tweets[i].text);
+ 	console.log("");
  }
 
 });
 
+}
+else if(command === "spotify-this-song" || command === "spotifythissong"){
 console.log("calling spotify rest endpoint");
-
-spotifyClient.search({type:"track",query:"greyhound"},function(error,trackdata){
+if(cmdparam === undefined || cmdparam === null){
+	cmdparam = "The Sign";
+}
+spotifyClient.search({type:"track",query:cmdparam},function(error,trackdata){
 	if(error){
 		console.log("error in response");
 	}
@@ -30,4 +39,6 @@ spotifyClient.search({type:"track",query:"greyhound"},function(error,trackdata){
     console.log("Song: " + trackdata.tracks.items[0].name);
 	console.log("Preview url: "+ trackdata.tracks.items[0].preview_url);
 
-})
+});
+
+}
